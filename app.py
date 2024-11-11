b1430ffea781164ca042d076c0bec6b3c414540a
shashika-pic <146179263+shashika-pic@users.noreply.github.com>
2024-11-11 12:00:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from fastapi import FastAPI
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route('/')
-def hello():
-    return "Hello, World!"
+@app.get("/")
+async def hello():
+    return {"message": "Hello, World!"}
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
